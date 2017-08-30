94455f0bbe39eaf53348a772fcd32206f6ac5425
Jack H <jackhe678@gmail.com>
2017-08-30 17:29:14 -0400

    added particles

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import Header from './components/header.js'
 import SocialButtons from './components/socialBtn'
+import Particles from 'react-particles-js';
 
 class App extends Component {
   render() {
@@ -10,6 +11,69 @@ class App extends Component {
       <div className="App">
         <Header />
         <SocialButtons />
+          <Particles style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            zIndex: -1
+          }}
+          params={{
+            particles: {
+              number: {
+                value: 400,
+                density: {
+                  enable: true,
+                  value_area: 800
+                }
+              },
+              line_linked: {
+                enable: false,
+                distance: 50,
+                color: "#ffffff",
+                opacity: 0.4,
+                width: 1
+              },
+              "shape": {
+                "type": "circle",
+                "stroke": {
+                  "width": 0,
+                  "color": "#000000"
+                },
+                polygon: {
+                  nb_sides: 7
+                }
+              }
+             },
+              "interactivity": {
+                "detect_on": "canvas",
+                "events": {
+                  "onhover": {
+                    "enable": true,
+                    "mode": "grab"
+                  },
+                  "onclick": {
+                    "enable": true,
+                    "mode": "push"
+                  },
+                  "resize": true
+                },
+                "modes": {
+                  "grab": {
+                    "distance": 200,
+                    "line_linked": {
+                      "opacity": 1
+                    }
+                  },
+                  "repulse": {
+                    "distance": 100,
+                    "duration": 0.4
+                  }
+                }
+              },
+              "retina_detect": true
+          }} />
       </div>
     );
   }
