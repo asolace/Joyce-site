aeca7c7a0d08acf7dbeef32134bdb34f1983e260
Jack H <jackhe678@gmail.com>
2017-08-30 21:31:11 -0400

    reposition btns

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { Jumbotron, Container } from 'reactstrap';
 
 const Header = props => {
   return (
-    <div>
-      <Jumbotron fluid>
-        <Container fluid>
-          <h1 className="display-3 rainbow-animation joyce-header">Joyce Wang</h1>
-          <p className="lead weird-april">April is soooo WEIRD!</p>
-        </Container>
-      </Jumbotron>
+    <div className="joyce-header">
+      <h1 className="display-3 rainbow-animation">Joyce Wang</h1>
+      <p className="lead weird-april">April is soooo WEIRD!</p>
     </div>
   );
 };
